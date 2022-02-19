11476fa8f1289f5acf208917ae64ca1ed97032b6
Shivam Sharma <96768755+Shivam-Sharma-Github@users.noreply.github.com>
2022-02-20 00:06:30 +0530

    Update play_with_array.js

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -1,16 +1,26 @@
 function getEven(arr) {
+    var res=[]
+    for (var i=0;i< arr.length;i++){
+        if(arr[i]%2===0){
+            res.push(arr[i]) ;
+        }
+    }
+    return res;
     /* 
         Return an array containing only even numbers as a result
         Example
             Input: [1,4,5,77,8,90]
             Output: [4,8,90]
-
         Write you code below
     */
 
 }
 
 function multiplyByN(arr, n) {
+    for (var i=0; i<arr.length; i++){
+        arr[i] = n*arr[i] ;
+    }
+    return arr;
     /* 
         Return an array whose elements are multiplied by n
         Example
@@ -21,6 +31,8 @@ function multiplyByN(arr, n) {
 }
 
 function removeNthElement(arr, n) {
+    arr.splice(n,1);
+    return arr;
     /*
         Return the array with the element present at index n removed
         Example
